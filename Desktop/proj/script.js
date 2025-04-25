1db0c0aa4421aca946fd1f6a1885b901ce139c80
Parth Mehta <mehtaparth101@gmail.com>
2025-04-25 01:08:21 -0700

    test feature

--- Desktop/proj/script.js
+++ Desktop/proj/script.js
@@ -114,3 +114,25 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     animate();
 });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const toggle = document.getElementById("darkToggle");
+//     const savedTheme = localStorage.getItem("theme");
+
+//     if (savedTheme === "dark") {
+//         document.documentElement.classList.add("dark");
+//         toggle.checked = true;
+//     } else {
+//         toggle.checked = false;
+//     }
+
+//     toggle.addEventListener("change", () => {
+//         if (toggle.checked) {
+//             document.documentElement.classList.add("dark");
+//             localStorage.setItem("theme", "dark");
+//         } else {
+//             document.documentElement.classList.remove("dark");
+//             localStorage.setItem("theme", "light");
+//         }
+//     });
+// });
